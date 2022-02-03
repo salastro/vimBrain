fb9fbe621844646bb0a7c27775d82b5c902f21d6
salahdin-ahmed <salah2112004@gmail.com>
2022-02-03 17:56:14 +0200

    Change from double to single quotes

--- vimBrainPy/vb.py
+++ vimBrainPy/vb.py
@@ -18,8 +18,8 @@ def interpreter(code):
     array, starts, user, loops, i, pointer = [0]*30000, [], [], {}, 0, 0
     for index, instruction in enumerate(code):
         match instruction:
-            case "q": starts.append(index)
-            case "@": start = starts.pop(); loops[start] = index; loops[index] = start
+            case 'q': starts.append(index)
+            case '@': start = starts.pop(); loops[start] = index; loops[index] = start
     while i < len(code):
         match code[i]:
             case 'h': pointer -= 1
@@ -27,7 +27,7 @@ def interpreter(code):
             case 'k': array[pointer] += 1
             case 'j': array[pointer] -= 1
             case 'p': print(chr(array[pointer]), end='')
-            case 'i': user = list(input() + "\n") if not user else user; array[pointer] = ord(user.pop(0))
+            case 'i': user = list(input() + '\n') if not user else user; array[pointer] = ord(user.pop(0))
             case 'q': i = loops[i] if not array[pointer] else i
             case '@': i = loops[i] if array[pointer] else i
         i += 1
