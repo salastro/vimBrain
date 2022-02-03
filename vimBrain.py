575f5a75ce138730a97d3c47036139329b1d99dd
salahdin-ahmed <salah2112004@gmail.com>
2022-02-03 05:41:27 +0200

    Fix loop implementation

--- vimBrain.py
+++ vimBrain.py
@@ -1,5 +1,9 @@
 def vimBrain(code):
-    array, starts, ends, i, pointer = [0], [], [], 0, 0
+    array, starts, loops, i, pointer = [0], [], {}, 0, 0
+    for index, instruction in enumerate(code):
+        match instruction:
+            case "q": starts.append(index)
+            case "@": start = starts.pop(); loops[start] = index; loops[index] = start
     while i < len(code):
         match code[i]:
             case 'h': pointer -= 1 if pointer > 0 else 0
@@ -18,6 +22,8 @@ def vimBrain(code):
             case '@':
                 ends.append(i); i = starts[-1]
                 if array[pointer] == 0: starts.pop(-1); i = ends[-1]
+            case 'q': i = loops[i] if not array[pointer] else i
+            case '@': i = loops[i] if array[pointer] else i
         i += 1
 
 
