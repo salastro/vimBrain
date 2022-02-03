147549d47082c011b51673ae7f96aa700212ded6
salahdin-ahmed <salah2112004@gmail.com>
2022-02-03 08:35:14 +0200

    Add license

--- vb.py
+++ vb.py
@@ -1,3 +1,19 @@
+# VB.PY: a python interpreter for a brainfuck-based vim-inspired esoteric
+# programming language, vimBrain
+# Copyright © 2022 SalahDin Ahmed Salh Rezk
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation version 2
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 def interpreter(code):
     array, starts, user, loops, i, pointer = [0]*30000, [], [], {}, 0, 0
     for index, instruction in enumerate(code):
