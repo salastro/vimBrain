d7b609ff715ce26a20053525919497db02f6c6e1
salahdin-ahmed <salah2112004@gmail.com>
2022-02-03 06:35:12 +0200

    Fix user input

--- vimBrain.py
+++ vimBrain.py
@@ -11,7 +11,7 @@ def interpreter(code):
             case 'k': array[pointer] += 1
             case 'j': array[pointer] -= 1
             case 'p': print(chr(array[pointer]), end='')
-            case 'i': list(input() + "\n") if user == [] else 0; array[pointer] = ord(user.pop(0))
+            case 'i': user = list(input() + "\n") if user == [] else user; array[pointer] = ord(user.pop(0))
             case 'q': i = loops[i] if not array[pointer] else i
             case '@': i = loops[i] if array[pointer] else i
         i += 1
